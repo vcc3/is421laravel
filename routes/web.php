8933c1418b1cb2839e1d3d586b433630aaee2224
Victor Carlos Cornejo <vcc3@njit.edu>
2017-10-11 19:23:59 -0400

    added about page

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,9 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Route::get('about',function(){
+	return  'about page';
+});
 Route::get('/', function () {
     return view('welcome');
 });
